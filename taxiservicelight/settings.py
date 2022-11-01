89f1994f3dfcc8dc69d18895cab97559a3ff1581
stoveprofi <stoveprofi@gmail.com>
2022-11-01 17:54:26 +0000

    Updates settings

--- taxiservicelight/settings.py
+++ taxiservicelight/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -19,11 +19,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = 'django-insecure-aoty=c&c@n++&ev8j)d#kql7zdmo=zrdbarx*o8h-@xt75u=as'
+#
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-aoty=c&c@n++&ev8j)d#kql7zdmo=zrdbarx*o8h-@xt75u=as'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+# SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-aoty=c&c@n++&ev8j)d#kql7zdmo=zrdbarx*o8h-@xt75u=as')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = []
 
