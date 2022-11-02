a001024f7166a670d1572d3ecd7282d4a7bb7a2d
stoveprofi <stoveprofi@gmail.com>
2022-11-02 10:27:52 +0000

    Updates settings, create statifiles

--- taxiservicelight/settings.py
+++ taxiservicelight/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 
+import dj_database_url
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,8 +28,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
-# SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-aoty=c&c@n++&ev8j)d#kql7zdmo=zrdbarx*o8h-@xt75u=as')
+# SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", 'django-insecure-aoty=c&c@n++&ev8j)d#kql7zdmo=zrdbarx*o8h-@xt75u=as')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
@@ -55,6 +57,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -96,6 +99,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# dj-database-url
+db_from_env = dj_database_url.config(conn_max_age=500)    # DATABASE_URL
+DATABASES["default"].update(db_from_env)
 
 
 # Password validation
@@ -140,7 +147,7 @@ STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (BASE_DIR / "static",)
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = "staticfiles/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
